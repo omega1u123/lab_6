e556e9cc7425ae9ff330ca50bec420209ba62ce9
omega1u123 <gvardian2810@gmail.com>
2023-03-06 02:45:41 +0300

    edited SaveNoteScreen.kt

---
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/components/screens/SaveNoteScreen.kt
@@ -0,0 +1,53 @@
+package com.raywenderlich.android.jetnotes.util.components.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.raywenderlich.android.jetnotes.domain.model.ColorModel
+import com.raywenderlich.android.jetnotes.util.components.NoteColor
+import com.raywenderlich.android.jetnotes.util.fromHex
+
+
+@Composable
+fun ColorItem(
+    color: ColorModel,
+    onColorSelect: (ColorModel) -> Unit
+){
+    Row(
+      modifier = Modifier
+          .fillMaxWidth()
+          .clickable(
+              onClick = {
+                  onColorSelect(color)
+              }
+          )
+    ){
+        NoteColor(
+            modifier = Modifier
+                .padding(10.dp), color = Color.fromHex(color.hex), size = 80.dp,
+            border = 2.dp
+        )
+        Text(
+           text = color.name,
+           fontSize = 22.sp,
+           modifier = Modifier
+               .padding(horizontal = 16.dp)
+               .align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ColorItemPreview(){
+    ColorItem(ColorModel.DEFAULT){}
+}
