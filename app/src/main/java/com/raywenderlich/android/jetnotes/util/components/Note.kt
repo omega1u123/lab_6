d8bc9508ffa5201ce32ac4171ed5ff4c5258e6eb
omega1u123 <gvardian2810@gmail.com>
2023-03-06 02:21:20 +0300

    edited Note.kt

--- app/src/main/java/com/raywenderlich/android/jetnotes/util/components/Note.kt
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/components/Note.kt
@@ -1,12 +1,16 @@
 package com.raywenderlich.android.jetnotes.util.components
 
+import android.graphics.drawable.shapes.Shape
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import com.raywenderlich.android.jetnotes.R
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,7 +20,15 @@ import com.raywenderlich.android.jetnotes.theme.rwGreen
 
 @Composable
 fun Note() {
-    Row(modifier = Modifier.fillMaxWidth()) {
+    val backgroundShape: RoundedCornerShape = RoundedCornerShape(4.dp)
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp, backgroundShape)
+            .fillMaxWidth()
+            .heightIn(min = 64.dp)
+            .background(Color.White, backgroundShape)
+    ) {
        NoteColor(
            color = rwGreen,
            size = 40.dp,
