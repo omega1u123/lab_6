299fd16821027d4df387b3afadbb6212876be2b3
omega1u123 <gvardian2810@gmail.com>
2023-03-06 02:36:01 +0300

    edited Note.kt

--- app/src/main/java/com/raywenderlich/android/jetnotes/util/components/Note.kt
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/components/Note.kt
@@ -13,8 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.jetnotes.theme.rwGreen
 
 
@@ -42,8 +45,24 @@ fun Note() {
             .weight(1f)
             .align(Alignment.CenterVertically)
         ) {
-            Text(text = stringResource(id = R.string.header), maxLines = 1)
-            Text(text = stringResource(id = R.string.content), maxLines = 1)
+            Text(text = stringResource(id = R.string.header),
+                color = Color.Black,
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 16.sp,
+                    letterSpacing = 0.15.sp
+                )
+                )
+            Text(text = stringResource(id = R.string.content),
+                color = Color.Black.copy(alpha = 0.75f),
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    letterSpacing = 0.25.sp
+                )
+            )
         }
         Checkbox(
             checked = false,
