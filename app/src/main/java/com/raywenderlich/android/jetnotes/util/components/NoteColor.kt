ac643add7b7f7c82d20c9e56433fa3f9c4b70886
omega1u123 <gvardian2810@gmail.com>
2023-03-06 02:03:00 +0300

    edited NoteColor.kt

---
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/components/NoteColor.kt
@@ -0,0 +1,26 @@
+package com.raywenderlich.android.jetnotes.util.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoteColor(){
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .background(Color.Red)
+    )
+}
+
+
+@Preview
+@Composable
+fun NoteColorPreview(){
+    NoteColor()
+}
