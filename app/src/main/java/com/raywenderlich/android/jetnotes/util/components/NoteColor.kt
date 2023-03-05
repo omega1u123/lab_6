998e9a54f902dfe11d296fb1b1eb48b69e6c9b63
omega1u123 <gvardian2810@gmail.com>
2023-03-06 02:04:51 +0300

    edited NoteColor.kt

--- app/src/main/java/com/raywenderlich/android/jetnotes/util/components/NoteColor.kt
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/components/NoteColor.kt
@@ -3,8 +3,10 @@ package com.raywenderlich.android.jetnotes.util.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,6 +17,8 @@ fun NoteColor(){
         modifier = Modifier
             .size(40.dp)
             .background(Color.Red)
+            .clip(CircleShape)
+            .background(Color.Yellow)
     )
 }
 
