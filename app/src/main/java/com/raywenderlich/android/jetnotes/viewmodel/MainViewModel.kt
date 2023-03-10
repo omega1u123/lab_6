4873a5157c434fc27ee89db8ee302b0247c9cb0e
omega1u123 <gvardian2810@gmail.com>
2023-03-10 06:31:59 +0300

    edited MainViewModel.kt

--- app/src/main/java/com/raywenderlich/android/jetnotes/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/raywenderlich/android/jetnotes/viewmodel/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.raywenderlich.android.jetnotes.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.raywenderlich.android.jetnotes.data.repository.Repository
+import com.raywenderlich.android.jetnotes.domain.model.NoteModel
 
 /**
  * View model used for storing the global app state.
@@ -9,5 +11,7 @@ import com.raywenderlich.android.jetnotes.data.repository.Repository
  * This view model is used for all screens.
  */
 class MainViewModel(private val repository: Repository) : ViewModel() {
-
+    val notesNotInTrash: LiveData<List<NoteModel>> by lazy{
+        repository.getAllNotesNotInTrash()
+    }
 }
